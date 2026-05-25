9abffb1f20f5ba10619f81f709a6acd75b514b3c
Dhananjai Chand <dcdevdhan@gmail.com>
2026-05-25 16:31:37 -0400

    Configure portfolio for custom domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,10 @@
-const isGithubActions = process.env.GITHUB_ACTIONS === "true";
-const repoName = process.env.GITHUB_REPOSITORY
-  ? process.env.GITHUB_REPOSITORY.split("/")[1]
-  : "";
-const basePath = isGithubActions && repoName ? `/${repoName}` : "";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   trailingSlash: true,
   images: {
     unoptimized: true
-  },
-  basePath,
-  assetPrefix: basePath ? `${basePath}/` : undefined
+  }
 };
 
 export default nextConfig;
